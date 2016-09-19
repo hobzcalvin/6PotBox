6f02c1539f8007018c79a1a4321e854f6629ba49
Grant Patterson <grant.patterson@gmail.com>
2016-09-18 20:49:34 -0700

    Stopping here for now
    Noise pot, combined bright/sat pot.

--- _6PotBox.ino
+++ _6PotBox.ino
@@ -33,6 +33,10 @@
 #define NUM_LEDS 100
 CRGB leds[NUM_LEDS];
 
+#define MIDDLE_TOL  12
+
+
+uint32_t last_ms;
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
@@ -57,36 +61,56 @@ void setup() {
   FastLED.show();
   delay(500);
 
-  Serial.begin(9600);
+  //Serial.begin(9600);
+
+  last_ms = millis();
 }
 
 
 int variance = 0;
-boolean doprints = true;
+boolean doprints = false;
+uint16_t noiseIn = 0;
 
 void loop() {
+  
   // Base hue, 14 bits
   word baseHue = analogRead(POT_HUE) << 4;
   // How far away we go from that hue, 14 bits (to allow for things greater than it)
   int hueVariance = analogRead(POT_HUEVAR) << 4;
   // What distance it takes to cycle back to the base hue
-  word hueWidth = sqrt(analogRead(POT_HUEWID)) * hueVariance / 255;
-  if (doprints) {
+  word hueWidth = sqrt16(analogRead(POT_HUEWID)) * hueVariance / 255;
+
+  // START HERE:
+  // When hueVariance is zero, speed does...something
+  // 
+  word brightsat = analogRead(POT_BRIGHT);
+  byte saturation;
+  if (brightsat < 512) {
+    FastLED.setBrightness(brightsat >> 1);
+    saturation = 255;
+  } else {
+    FastLED.setBrightness(255);
+    saturation = (1023 - brightsat) >> 1;
+  }
+  uint16_t noNoise = 255 - (analogRead(POT_SAT) >> 2);
+  
+  int spd = analogRead(POT_SPEED);
+  int speed_adder = 0;
+  if (spd < 512 - MIDDLE_TOL) {
+    speed_adder = 512 - MIDDLE_TOL - spd;
+  } else if (spd > 512 + MIDDLE_TOL) {
+    speed_adder = 512 + MIDDLE_TOL - spd;
+  }
+  noiseIn += speed_adder / (FPS/4);
+
+  /*if (doprints) {
     Serial.print(">>> basehue ");
     Serial.print(baseHue);
     Serial.print(" huewid ");
     Serial.print(hueWidth);
     Serial.print(" huevar ");
     Serial.print(hueVariance);
-  }
-
-  // START HERE:
-  // When hueVariance is zero, speed does...something
-  // Sat/bright pots combined??
-  // 
-
-  byte saturation = analogRead(POT_SAT) >> 2;
-  FastLED.setBrightness(analogRead(POT_BRIGHT) >> 2);
+  }*/
   
   while (variance < 0) variance += hueVariance*2;
   while (hueVariance > 0 && variance > (hueVariance*2)) variance -= hueVariance*2;
@@ -98,14 +122,14 @@ void loop() {
     //hue = ((variance < hueVariance) ? (variance) : ((hueVariance << 1) - variance)); // 5 shifts from analog
     //hue = (baseHue + (variance < hueVariance) ? (variance) : ((hueVariance << 1) - variance)) >> 7; // 5 shifts from analog
     //hue = (baseHue + (((variance < hueVariance) ? (variance) : ((hueVariance << 1) - variance)) << 2)) >> 9;
-    if (i == 0 && doprints) {
+    /*if (i == 0 && doprints) {
       Serial.print(" curvar ");
       Serial.print(curVariance);
       Serial.print(" thing ");
       Serial.print(((curVariance < hueVariance) ? (curVariance) : ((hueVariance << 1) - curVariance)));
       Serial.print(" hue ");
       Serial.println(hue);
-    }
+    }*/
     /*Serial.print(variance);
     Serial.print('=');
     Serial.print(hue);
@@ -117,7 +141,19 @@ void loop() {
     Serial.print(hue);*/
     //Serial.print(',');
     if (saturation) {
-      leds[i] = CHSV(hue, saturation, 255);
+      /*uint16_t ms = millis() >> 1;
+      byte brt = inoise8(ms, i<<7);//inoise8(variance >> 4, uint16_t(i) << 4);*/
+
+      byte brt = inoise8(noiseIn, i << 4, 0);
+      byte srt = inoise8(noiseIn, i << 4, 0xF000);
+      /*if (i == 0) {
+        Serial.print(noNoise);
+        Serial.print(' ');
+        Serial.print(noiseIn);
+        Serial.print(' ');
+        Serial.println(brt);
+      }*/
+      leds[i] = CHSV(hue, min(max(noNoise, uint16_t(srt)*2), saturation), max(noNoise, uint16_t(brt)));
     } else {
       // Special case: if saturation is zero (all white), use hue for brightness
       leds[i] = CHSV(0, 0, hue);
@@ -126,12 +162,8 @@ void loop() {
     curVariance += hueWidth;
     while (curVariance > hueVariance*2) curVariance -= hueVariance*2;
   }
-  word spd = analogRead(POT_SPEED);
-  if (spd < 500 || spd > 524) {
-    int thing = (analogRead(POT_SPEED) - 512) * 200 / FPS;
-    variance += thing;
-    if (doprints) Serial.println(thing);
-  }
+  // Increment/decrement variance for next time
+  variance += speed_adder * 200 / FPS;
   //Serial.println("\n");
   FastLED.show();
   FastLED.delay(1000/FPS);
