e24f5f904e7dfac419be085a79d6d0e42178312c
Grant Patterson <grant.patterson@gmail.com>
2017-01-14 13:31:06 -0800

    Lots of changes after meltdown/redo
    I needed to rewire everything, so there are new potentiometer mappings.
    A variety of tweaks made to the noise behavior (mostly) which wanted
    some improved debugging, so add some of that stuff too and turn it
    on/off with a constant.
    This totally works and was fun to play with!!!

--- _6PotBox.ino
+++ _6PotBox.ino
@@ -1,25 +1,25 @@
 #include <FastLED.h>
 
+
 /*
  * Potentiometer Layout:
  * 
- *   A4=hue
- *                A5=sat
+ *   A9=hue
+ *                A4=noise
  *   A8=hueVar
- *                A7=speed
- *   A9=hueWidth
- *                A6=bright
+ *                A5=speed
+ *   A7=hueWidth
+ *                A6=brightsat
  *       
  *     ||  <-- cable out  
  *     ||
  */
 
-
-#define POT_HUE     A4
+#define POT_HUE     A9
 #define POT_HUEVAR  A8
-#define POT_HUEWID  A9
-#define POT_NOISE     A5
-#define POT_SPEED   A7
+#define POT_HUEWID  A7
+#define POT_NOISE     A4
+#define POT_SPEED   A5
 #define POT_BRIGHTSAT  A6
 
 #define LED_PIN     13
@@ -35,10 +35,16 @@ CRGB leds[NUM_LEDS];
 
 #define MIDDLE_TOL  12
 
+#define DEBUG false
 
 uint32_t last_ms;
 
 void setup() {
+  if (DEBUG) {
+    Serial.begin(9600);
+    Serial.println("HELLO WORLD!");
+  }
+
   pinMode(LED_PIN, OUTPUT);
   pinMode(DATA_PIN, OUTPUT);
   FastLED.addLeds<LED_SETTINGS>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
@@ -62,25 +68,41 @@ void setup() {
   delay(500);
 
   random16_set_seed(analogRead(POT_SPEED));
-  //Serial.begin(9600);
 
   last_ms = millis();
 }
 
 
-int variance = 0;
-boolean doprints = false;
+int32_t variance = 0;
 uint16_t noiseIn = 0;
 boolean reversed = false;
 
 void loop() {
+/*
+  Serial.print(analogRead(POT_HUE));
+  Serial.print("\t");
+  Serial.print(analogRead(POT_HUEVAR));
+  Serial.print("\t");
+  Serial.print(analogRead(POT_HUEWID));
+  Serial.print("\t");
+  Serial.print(analogRead(POT_NOISE));
+  Serial.print("\t");
+  Serial.print(analogRead(POT_SPEED));
+  Serial.print("\t");
+  Serial.println(analogRead(POT_BRIGHTSAT));
+  return;
+*/
+  
   
   // Base hue, 14 bits
   word baseHue = analogRead(POT_HUE) << 4;
+  //if (DEBUG) { Serial.print(baseHue); Serial.print("\t"); }
   // How far away we go from that hue, 14 bits (to allow for things greater than it)
   int hueVariance = analogRead(POT_HUEVAR) << 4;
+  //if (DEBUG) { Serial.print(hueVariance); Serial.print("\t"); }
   // What distance it takes to cycle back to the base hue
   word hueWidth = sqrt16(analogRead(POT_HUEWID)) * hueVariance / 255;
+  //if (DEBUG) { Serial.print(hueWidth); Serial.print("\t"); }
 
   // START HERE:
   // When hueVariance is zero, speed does...something
@@ -97,12 +119,15 @@ void loop() {
   uint16_t noNoise;
   uint16_t reverseProb;
   int potNoise = analogRead(POT_NOISE);
+  if (DEBUG) { Serial.print("pn"); Serial.print(potNoise); }
   if (potNoise > 511) {
     noNoise = 255 - byte(potNoise >> 1);
     reverseProb = 0;
+    if (DEBUG) { Serial.print("\tnnz"); Serial.print(noNoise); }
   } else {
     noNoise = 255;
     reverseProb = 511 - potNoise;
+    if (DEBUG) { Serial.print("\trev"); Serial.print(reverseProb); }
   }
   
   int spd = analogRead(POT_SPEED);
@@ -133,9 +158,13 @@ void loop() {
     Serial.print(" huevar ");
     Serial.print(hueVariance);
   }*/
-  
-  while (variance < 0) variance += hueVariance*2;
-  while (hueVariance > 0 && variance > (hueVariance*2)) variance -= hueVariance*2;
+
+
+  //if (DEBUG) { Serial.print(variance); Serial.print("=>"); }
+  int hack = 0;
+  while (variance < 0 && hack++ < 50) variance += hueVariance*2;
+  while (hueVariance > 0 && variance > (hueVariance*2) && hack++ < 50) variance -= hueVariance*2;
+  //if (DEBUG) { Serial.print(variance); Serial.println(); }
   int curVariance = variance;
   byte hue;
   for (int i = 0; i < NUM_LEDS; i++) {
@@ -163,11 +192,26 @@ void loop() {
     Serial.print(hue);*/
     //Serial.print(',');
     if (saturation) {
-      if (noNoise < 255) {
-        byte brt = inoise8(noiseIn, i << 4, 0);
-        byte srt = inoise8(noiseIn, i << 4, 0xF000);
-        leds[i] = CHSV(hue, min(max(noNoise, uint16_t(srt)*2), saturation), max(noNoise, uint16_t(brt)));
+      if (potNoise > 511) {
+        int hueplay = int(inoise8(noiseIn*4, i << 4, 0)) - 127;
+        if (DEBUG && !i) { Serial.print("\thp?"); Serial.print(hueplay); }
+        if (hueplay > 0 && hueplay > (255-noNoise)) {
+          hueplay = 255 - noNoise;
+        } else if (hueplay < 0 && -hueplay > (255-noNoise)) {
+          hueplay = -(255 - noNoise);
+        }
+        uint16_t srt = inoise8(noiseIn, i << 4, 0xF000) * 2 - 100;
+        srt = max(noNoise, srt);
+        srt = min(srt, saturation);
+        if (DEBUG && !i) {
+          Serial.print("\thp"); Serial.print(hueplay);
+          Serial.print("\th"); Serial.print(hue);
+          Serial.print("\thh"); Serial.print((int(hue) + hueplay + 256) % 256);
+          Serial.print("\tsrt"); Serial.println(srt);
+        }
+        leds[i] = CHSV((int(hue) + hueplay + 256) % 256, srt, 255);
       } else {
+        if (DEBUG && !i) { Serial.println(); }
         leds[i] = CHSV(hue, saturation, 255);
       }
     } else {
